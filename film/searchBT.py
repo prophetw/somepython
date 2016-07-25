3c3449768f936fe1e9401d9327aa4c7fca7be104
Wang Wei <weiw_a@sumilux.cn>
2016-07-25 09:32:52 +0800

    raw format save to our file then we can download the torrent

--- film/searchBT.py
+++ film/searchBT.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import requests as rs
 import re
 import json
-query = '火星救援'
+import os
+query = '葫芦娃'
 params = {
     'q':query,
     'sitesearch':'www.bttiantang.com',
@@ -19,12 +20,12 @@ headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36'
 }
 q = s.get(searchBTURL,params=params,headers=headers)
-print q.url
+print(q.url)
 html = q.text
 soup = BeautifulSoup(html,'lxml')
 soup.prettify()
 q = soup.find_all('div',class_='litpic')
-print q[0].find('a')
+print(q[0].find('a'))
 result = q[0].find('a')
 filmbtpage = result.get('href')
 BTurl = baseURL+filmbtpage
@@ -39,20 +40,42 @@ totalbttitles = []
 for links in btlinks:
     totalbtlinks.append(links.find('a').get('href'))
     totalbttitles.append(links.find('a')['title'])
-print totalbtlinks
-print totalbttitles[0]
+print(totalbtlinks)
+print(totalbttitles)
 whatineed = s.get(baseURL+totalbtlinks[0])
-print whatineed.url
+print(whatineed.url)
 
+urlParams = whatineed.url
+def getId(urlParams):
+    idRegular = r'id=(\d+)'
+    result = re.search(idRegular,urlParams)
+    id = result.group(1)
+    return id
+def getHash(urlParams):
+    idRegular = r'uhash=(.+)'
+    result = re.search(idRegular, urlParams)
+    hash = result.group(1)
+    return hash
+
+id= getId(urlParams)
+# print(id)
+hash= getHash(urlParams)
+# print(hash)
 # 最后一个 post 请求
 # 链接 http://www.bttiantang.com/download1.php
 downloadURL = 'http://www.bttiantang.com/download1.php'
 payload = {
     'action':'download',
-    'id':'20905',
-    'uhash':'58c8b2eb531f2cb76bedf4a7',
+    'id':id,
+    'uhash':hash,
     'imageField.x':'40',
     'imageField.y':'32'
 }
-bt = s.post(downloadURL,data=payload)
-# print bt.text
+bt = s.post(downloadURL,data=payload,stream=True)
+# print(bt.text)
+print(bt.raw)
+file = open(query+'.torrent', 'w')
+with open(query+'.torrent', 'wb') as fd:
+    for chunk in bt.iter_content(10):
+        fd.write(chunk)
+fd.close()
